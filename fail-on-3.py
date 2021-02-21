e3ce0daa3b385d8e58f6df4545a737a5b79f9dad
bennybe <bennybe@gmail.com>
2021-02-21 12:42:10 +0200

    Added first job

--- fail-on-3.py
+++ fail-on-3.py
@@ -1,6 +1,6 @@
 import os, sys
 
-print(os.environ.get('BUILD_ID'))
+print( 'This is a build number {} of JOB: {}'.format(os.environ.get('BUILD_ID'), os.environ.get('JOB_NAME')))
 if (int(os.environ.get('BUILD_ID')) % 3) != 0:
     print("Fail!!!")
     sys.exit(1)
